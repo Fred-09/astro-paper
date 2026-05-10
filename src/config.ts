93ba03cbae231229339ee14bf72bee6783cd75a8
Fred-09 <2769241966@qq.com>
2026-05-10 18:58:16 +0800

    first version

--- src/config.ts
+++ src/config.ts
@@ -4,7 +4,7 @@ export const SITE = {
   profile: "",
   desc: "A personal blog about life, learning and thinking.",
   title: "Fred Song",
-  ogImage: "astropaper-og.jpg",
+  ogImage: "head.svg",
   lightAndDarkMode: true,
   postPerIndex: 4,
   postPerPage: 4,
