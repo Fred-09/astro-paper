2ba9a934b71207342ee5090a13bef989d19071f2
Fred-09 <2769241966@qq.com>
2026-06-18 22:57:45 +0800

    optimizing

--- astro.config.ts
+++ astro.config.ts
@@ -1,4 +1,4 @@
-import { defineConfig, envField, fontProviders } from "astro/config";
+import { defineConfig, envField } from "astro/config";
 import tailwindcss from "@tailwindcss/vite";
 import sitemap from "@astrojs/sitemap";
 import remarkToc from "remark-toc";
@@ -59,22 +59,5 @@ export default defineConfig({
   },
   experimental: {
     preserveScriptOrder: true,
-    fonts: [
-      {
-        name: "Inter",
-        cssVariable: "--font-inter",
-        provider: fontProviders.local(),
-        fallbacks: ["monospace"],
-        options: {
-          variants: [
-            { weight: 300, style: "normal", src: ["public/fonts/inter-300-normal.ttf"] },
-            { weight: 400, style: "normal", src: ["public/fonts/inter-400-normal.ttf"] },
-            { weight: 500, style: "normal", src: ["public/fonts/inter-500-normal.ttf"] },
-            { weight: 600, style: "normal", src: ["public/fonts/inter-600-normal.ttf"] },
-            { weight: 700, style: "normal", src: ["public/fonts/inter-700-normal.ttf"] },
-          ],
-        },
-      },
-    ],
   },
 });
